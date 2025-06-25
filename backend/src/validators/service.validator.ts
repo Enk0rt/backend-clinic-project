8ca0ed07e1241e423b8eb21e96f5cd508f58bf41
Enk0rt <delovoyartem4ick@gmail.com>
2025-06-25 20:24:33 +0300

    feat: add postman collection

--- backend/src/validators/service.validator.ts
+++ backend/src/validators/service.validator.ts
@@ -4,7 +4,7 @@ import { zod } from "../zod";
 export class ServiceValidator {
     public static createOrUpdate = zod.object({
         name: zod
-            .string()
+            .string({ required_error: "Name is required" })
             .min(2, "Name must be at least 2 characters")
             .max(100, "Name must be at most 100 characters")
             .regex(new RegExp(RegexEnums.SERVICE_NAME), "Invalid name format")
